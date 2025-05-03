0ce6d86a5f887d3c7f83ec9cfa4087125f738591
mohammed ammourie <mohammed.ammourie@gmail.com>
2025-05-03 20:21:56 +0300

    modifications

--- lib/features/payment_records/view_model/payment_record_view_model.dart
+++ lib/features/payment_records/view_model/payment_record_view_model.dart
@@ -1,15 +1,15 @@
 import 'dart:developer';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:slim_starter_application/core/common/view_model/base_view_model.dart';
+import 'package:slim_starter_application/core/ui/error_ui/error_viewer/error_viewer.dart';
 import 'package:slim_starter_application/core/ui/snackbars/show_snackbar.dart';
 import 'package:slim_starter_application/core/ui/widgets/success_dialog.dart';
 import 'package:slim_starter_application/features/home/view/app_main_view.dart';
 
 import '../../../core/common/app_config.dart';
 import '../../../core/constants/enums/success_dialog_type.dart';
+import '../../../services/firebase_service.dart';
 import '../model/enums/payment_type.dart';
 import '../model/response/customer_model.dart';
 import '../model/response/payment_record_model.dart';
@@ -17,8 +17,7 @@ import '../view/payment_record_view.dart';
 
 class PaymentRecordViewModel extends BaseViewModel<PaymentRecordViewParam> {
   final PaymentRecordViewParam param;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final PaymentFirebaseDataSource _firebaseDataSource;
 
   List<CustomerModel> _customers = [];
   bool _isLoadingCustomers = false;
@@ -37,7 +36,9 @@ class PaymentRecordViewModel extends BaseViewModel<PaymentRecordViewParam> {
   final TextEditingController customerNameController = TextEditingController();
   final TextEditingController customerPhoneController = TextEditingController();
 
-  PaymentRecordViewModel(this.param) : super(param) {
+  PaymentRecordViewModel(this.param)
+      : _firebaseDataSource = PaymentFirebaseDataSource(),
+        super(param) {
     _init();
   }
 
@@ -93,89 +94,57 @@ class PaymentRecordViewModel extends BaseViewModel<PaymentRecordViewParam> {
     notifyListeners();
   }
 
-  // Firestore Operations
-  String get _userId => _auth.currentUser?.uid ?? '';
-
   Future<void> fetchCustomers() async {
-    if (_userId.isEmpty) {
-      showSnackbar("You must be logged in to view customers");
-      return;
-    }
+    final result = await _firebaseDataSource.getCustomers();
 
-    try {
-      final customersSnapshot = await _firestore
-          .collection('pro_users')
-          .doc(_userId)
-          .collection('customers')
-          .get();
-
-      _customers = customersSnapshot.docs
-          .map((doc) => CustomerModel.fromMap(doc.data()))
-          .toList();
-
-      // Set initial selected customer if list is not empty
-      if (_customers.isNotEmpty) {
-        _selectedCustomer = _customers[0];
-      } else {
-        _selectedCustomer = null;
-      }
-
-      notifyListeners();
-    } catch (e) {
-      print("Error fetching customers: $e");
-      showSnackbar("Error loading customers: $e");
-    }
+    result.pick(
+      onData: (customers) {
+        _customers = customers;
+        if (_customers.isNotEmpty) {
+          _selectedCustomer = _customers[0];
+        } else {
+          _selectedCustomer = null;
+        }
+        notifyListeners();
+      },
+      onError: (error) {
+        ErrorViewer.showError(
+          context: AppConfig().appContext!,
+          error: error,
+          callback: () => fetchCustomers(),
+        );
+      },
+    );
   }
 
   Future<void> addCustomer(CustomerModel customer) async {
-    if (_userId.isEmpty) {
-      showSnackbar("You must be logged in to add a customer");
-      return;
-    }
-
     _isAddingCustomer = true;
     notifyListeners();
 
-    try {
-      // Generate a new document ID
-      final docRef = _firestore
-          .collection('pro_users')
-          .doc(_userId)
-          .collection('customers')
-          .doc();
+    final result = await _firebaseDataSource.addCustomer(customer);
 
-      // Update customer with the generated ID
-      final customerWithId = CustomerModel(
-        id: docRef.id,
-        name: customer.name,
-        phone: customer.phone,
-      );
+    result.pick(
+      onData: (customerWithId) {
+        _customers.add(customerWithId);
+        _selectedCustomer = customerWithId;
+        notifyListeners();
+        showSnackbar("Customer added successfully");
+      },
+      onError: (error) {
+        ErrorViewer.showError(
+          context: AppConfig().appContext!,
+          error: error,
+          callback: () => addCustomer(customer),
+        );
+      },
+    );
 
-      // Save to Firestore
-      await docRef.set(customerWithId.toMap());
-
-      // Add to local list
-      _customers.add(customerWithId);
-      _selectedCustomer = customerWithId;
-
-      notifyListeners();
-      showSnackbar("Customer added successfully");
-    } catch (e) {
-      print("Error adding customer: $e");
-      showSnackbar("Error adding customer: $e");
-    } finally {
-      _isAddingCustomer = false;
-      notifyListeners();
-    }
+    _isAddingCustomer = false;
+    notifyListeners();
   }
 
   Future<void> submitPaymentRecord() async {
     log('Starting payment record submission', name: 'submitPaymentRecord');
-    if (_userId.isEmpty) {
-      log('Error: User not logged in', name: 'submitPaymentRecord');
-      showSnackbar("You must be logged in to submit a payment record");
-      return;
-    }
 
     if (_selectedCustomer == null) {
       log('Error: No customer selected', name: 'submitPaymentRecord');
@@ -203,44 +172,38 @@ class PaymentRecordViewModel extends BaseViewModel<PaymentRecordViewParam> {
 
         log('Created payment record model', name: 'submitPaymentRecord');
 
-        // Save to Firestore
-        log('Saving to Firestore', name: 'submitPaymentRecord');
-        final docRef = await _firestore
-            .collection('pro_users')
-            .doc(_userId)
-            .collection('payment_records')
-            .add(paymentRecord.toMap());
+        final result =
+            await _firebaseDataSource.submitPaymentRecord(paymentRecord);
 
-        // Update the ID with Firestore's generated ID
-        log('Updating record with Firestore ID: ${docRef.id}',
-            name: 'submitPaymentRecord');
-        await docRef.update({'id': docRef.id});
-        log('Navigating back', name: 'submitPaymentRecord');
-        showSuccessDialog(
-          title: "Payment Record Submitted",
-          content: "Payment record submitted successfully",
-          buttonText: "OK",
-          type: SuccessDialogType.type2,
-          showConfetti: true,
-          onButtonPressed: () {
-            Navigator.popUntil(
-              AppConfig().appContext!,
-              ModalRoute.withName(AppMainView.routeName),
+        result.pick(
+          onData: (updatedRecord) {
+            showSuccessDialog(
+              title: "Payment Record Submitted",
+              content: "Payment record submitted successfully",
+              buttonText: "OK",
+              type: SuccessDialogType.type2,
+              showConfetti: true,
+              onButtonPressed: () {
+                Navigator.popUntil(
+                  AppConfig().appContext!,
+                  ModalRoute.withName(AppMainView.routeName),
+                );
+              },
+            );
+            log('Payment record submitted successfully',
+                name: 'submitPaymentRecord');
+          },
+          onError: (error) {
+            ErrorViewer.showError(
+              context: AppConfig().appContext!,
+              error: error,
+              callback: () => submitPaymentRecord(),
             );
           },
         );
-        // Nav.pop();
-
-        log('Payment record submitted successfully',
-            name: 'submitPaymentRecord');
-      } catch (e) {
-        log('Error submitting payment record: $e', name: 'submitPaymentRecord');
-        showSnackbar("Error submitting payment record: $e", isError: true);
       } finally {
         _isSubmitting = false;
-
         notifyListeners();
-        // log('Submission process completed', name: 'submitPaymentRecord');
       }
     } else {
       log('Form validation failed', name: 'submitPaymentRecord');
